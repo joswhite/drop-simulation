bcaf6d2698e84dac42edd15ff33fa4b1c04fab44
Joseph White <joseph.white@microfocus.com>
2019-06-26 19:02:52 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {WebGLComponent} from './webgl/WebGLComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Web GL Tutorial</h1>
+      </header>
+      <WebGLComponent />
+    </div>
+  );
+}
+
+export default App;
